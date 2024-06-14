f114bebe7bcf1588edfc832e9f2aedd30eac7a70
Martin Čapek <87175835+capek-martin@users.noreply.github.com>
2024-06-14 12:53:24 +0200

    fix: relocate styled-components registry to utils

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { PlanetProvider } from "./contexts/planetContext";
-import StyledComponentsRegistry from "./contexts/registry";
+import StyledComponentsRegistry from "./utils/registry";
 import { LoadingProvider } from "./contexts/loadingContext";
 import GlobalStyle from "./globalStyles";
 import { ToastContainer } from "react-toastify";
